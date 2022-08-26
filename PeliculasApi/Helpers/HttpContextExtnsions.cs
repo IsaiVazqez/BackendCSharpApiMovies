d3abe592cada1fde4d7e50534d4f94e166c732ae
Isaí Vázquez Valdez <7yuvryi@gmail.com>
2022-08-25 23:27:13 -0500

    Now you can paginate and mannage how many actors you want to see in a get petition

---
+++ PeliculasApi/Helpers/HttpContextExtnsions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PeliculasApi.Helpers
+{
+    public static class HttpContextExtnsions
+    {
+        public async static Task InsertarParametrosPaginacion<T>(this HttpContext httpContext, 
+            IQueryable<T> queryable, int cantidadRegistrosPorPagina)
+        {
+            double cantidad = await queryable.CountAsync();
+
+            double cantidadPaginas = Math.Ceiling(cantidad / cantidadRegistrosPorPagina);
+
+            httpContext.Response.Headers.Add("cantidadPaginas", cantidadPaginas.ToString());
+        }
+    }
+}
