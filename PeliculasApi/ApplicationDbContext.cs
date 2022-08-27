a701816b882647ac26046f76e1c28a74b2014c44
Isaí Vázquez Valdez <7yuvryi@gmail.com>
2022-08-27 00:29:38 -0500

    Many to many relationship is created and the database was updated

--- PeliculasApi/ApplicationDbContext.cs
+++ PeliculasApi/ApplicationDbContext.cs
@@ -5,12 +5,23 @@ namespace PeliculasApi
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions options) :base(options)
+        public ApplicationDbContext(DbContextOptions options) : base(options)
         {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PeliculasActores>()
+                .HasKey(x => new { x.ActorId, x.PeliculaId });
+
+            modelBuilder.Entity<PeliculasGeneros>()
+                .HasKey(x => new { x.GeneroId, x.PeliculaId });
+
+            base.OnModelCreating(modelBuilder);
         }
         public DbSet<Genero> Generos { get; set; }
         public DbSet<Actor> Actores { get; set; }
-
         public DbSet<Pelicula> Peliculas { get; set; }
+        public DbSet<PeliculasActores> PeliculasActores { get; set; }
+        public DbSet<PeliculasGeneros> PeliculasGeneros { get; set; }
     }
 }
---
+++ PeliculasApi/Entities/PeliculasGeneros.cs
@@ -0,0 +1,13 @@
+﻿namespace PeliculasApi.Entities
+{
+    public class PeliculasGeneros
+    {
+        public int GeneroId { get; set; }
+
+        public int PeliculaId { get; set; }
+
+        public Genero Genero { get; set; }
+
+        public Pelicula Pelicula { get; set; }
+    }
+}
