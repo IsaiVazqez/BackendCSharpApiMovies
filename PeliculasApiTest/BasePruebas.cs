415bc66418578437d1b5a18284d4992fc41b4232
Isaí Vázquez Valdez <7yuvryi@gmail.com>
2022-09-08 22:30:55 -0500

    The project now have unit test for Gendres

---
+++ PeliculasApiTest/BasePruebas.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using NetTopologySuite;
+using PeliculasApi;
+using PeliculasApi.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeliculasApiTest
+{
+    public class BasePruebas
+    {
+        protected ApplicationDbContext ConstruirContext(string nombreDb)
+        {
+            var opciones = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(nombreDb).Options;
+
+            var dbContext = new ApplicationDbContext(opciones);
+
+            return dbContext;
+        }
+
+        protected IMapper ConfigurarAutoMapper()
+        {
+            var config = new MapperConfiguration(options =>
+            {
+                var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
+                options.AddProfile(new AutoMapperProfile(geometryFactory));
+            });
+
+            return config.CreateMapper();
+        }
+    }
+}
